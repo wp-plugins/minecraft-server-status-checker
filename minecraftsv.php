ee07d8c843ec5c593eb478b230f3a150343db6f6
verblick <verblick@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-20 19:10:33 +0000

    v0.1.1
    git-svn-id: https://plugins.svn.wordpress.org/minecraft-server-status-checker/tags/0.1.1@546684 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ minecraftsv.php
@@ -0,0 +1,81 @@
+<?php
+	/*
+	Plugin Name: MinecraftSV Status
+	Plugin URI: http://www.minecraftsv.com
+	Description: Plugin for displaying Minecraft Server status
+	Author: MinecraftDL
+	Version: 0.1
+	Author URI: http://www.minecraftdl.com
+	*/
+	
+	define('PLUGIN_URL', plugin_dir_url( __FILE__ ));
+	
+	add_action("widgets_init", array('mcsv_status', 'register'));
+	register_activation_hook( __FILE__, array('mcsv_status', 'activate'));
+	register_deactivation_hook( __FILE__, array('mcsv_status', 'deactivate'));
+	class mcsv_status {
+	function activate(){
+			$data = array( 'option1' => 'Default value' ,'option2' => 55);
+			if ( ! get_option('mcsv_status')){
+				add_option('mcsv_status' , $data);
+			} else {
+			  	update_option('mcsv_status' , $data);
+			}
+	}
+	function deactivate(){
+	    	delete_option('mcsv_status');
+	}
+	function control(){
+	    	$data = get_option('mcsv_status');
+		?>
+	    	<p><label>IP : <input name="server_ip" type="text" value="<?php echo $data['server_ip']; ?>" /></label></p>
+	    	<p><label>Port (25565) : <input name="server_port" size="5"  type="text" value="<?php echo $data['server_port']; ?>" /></label></p>
+		<?php
+			if (isset($_POST['server_ip'])){
+			$data['widget_title'] = attribute_escape($_POST['server_ip']);
+			$data['server_ip'] = attribute_escape($_POST['server_ip']);
+			$data['server_port'] = attribute_escape($_POST['server_port']);
+			update_option('mcsv_status', $data);
+		}
+	}
+	function widget($args){
+	  
+	  		
+	  
+	  		wp_enqueue_style('mcsv', PLUGIN_URL.'/css/mcsv_button.css');
+	  
+	  		$data = get_option('mcsv_status');
+	  		$title = $data['widget_title';
+	  		$port = $data['server_port'];
+	  		if(empty($port)) {
+	  			$port = "25565";
+	  		}
+	  		
+			echo $args['before_widget'];
+			if(!empty($title)) {
+			echo $args['before_title'] . $title . $args['after_title'];
+			}
+			// Check Server Status
+			
+			$online = @fsockopen($data['server_ip'], $port, $errno, $errstr, 1);
+			
+			echo "<span class='mcsv_status'> IP : ".$data['server_ip']."</span>";
+			
+			if($online) {
+				echo '<a href="#" class="btn btn-success" rel="tooltip" title="first tooltip">Server is Online</a>';
+			}else{
+				echo '<a href="#" class="btn btn-danger">Server is Offline</a>';
+			}
+		
+			
+			// End
+			echo $args['after_widget'];
+	  }
+	  function register(){
+			register_sidebar_widget('MinecraftSV Status', array('mcsv_status', 'widget'));
+			register_widget_control('MinecraftSV Status', array('mcsv_status', 'control'));
+	  }
+	}
+	
+	
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Minecraft Server Status Checker ===
+Contributors: Verblick
+Donate link: http://plugins.minecraftsv.com
+Tags: minecraft, server, status, checker, bukkit, vanilla
+Requires at least: 2.5
+Tested up to: 3.3.2
+Stable tag: 0.1.1
+
+== Description ==
+
+This plugin will detect and show the Minecraft Server Status. Works with any kinds of server.
+
+== Installation ==
+
+Use WordPress' Add New Plugin feature, searching "Minecraft Server Status Checker (MSSC)", or download the archive and:
+
+1. Upload `minecraftsv` directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to Widgets under Appearance and start playing with our widget!
+
+== Frequently Asked Questions ==
+
+= The plugin doesn't work, why? =
+
+Try de-activate and activate it again
+
+
+== Screenshots ==
+
+== Changelog ==
+
+= v0.1.1 - May 21, 2012 =
+* Fixed widget title bug
+
+= v0.1 - May 19, 2012 =
+* Start version
+
+== Template tags ==
+
+Coming soon in the next version.
